f80993ec74d39dc29feb46a30956847d08addfd3
Benjamin LAMARD <benjamin.lamard@etu.univ-lyon1.fr>
2015-12-30 18:45:27 +0100

    Ajout fenêtre principale

--- Tennis_JAVA/src/app/Tennis_JAVA.java
+++ Tennis_JAVA/src/app/Tennis_JAVA.java
@@ -1,21 +1,13 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package app;
 
-/**
- *
- * @author p1402517
- */
 public class Tennis_JAVA {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new App().setVisible(true);
+            }
+        });
     }
     
 }
